cfb37dbf010bcd7c9e600368ccce985d9b6bdf6b
Ben Willis <benjamin.willis@gmail.com>
2018-11-28 22:20:58 -0800

    Rescue and ignore missing database
    Fixes #7
    
    This seems similar approach to a similar problem solved in 666da9b05aa98061606148125204a57d04322fa6.

---
+++ spec/examples/missing_database_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe TrackHistory do
+
+  before(:all) do
+    ActiveRecord::Base.establish_connection(:adapter => 'postgresql', :database => '')
+
+    TrackHistory.disable_warnings
+    class AnonUser < ActiveRecord::Base
+      validates_length_of :name, :minimum => 2
+      track_history
+    end
+  end
+
+  after(:all) do
+    ActiveRecord::Base.establish_connection(:adapter => 'postgresql', :database => 'track_history')
+  end
+
+
+  it 'should not error out when there is no table for a history model' do
+    AnonUser.should_not respond_to(:historical_fields)
+  end
+
+end
